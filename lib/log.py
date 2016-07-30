73a152f35100eb03ee349e17602646b8dc745391
fengbaoli <wenyefbl@163.com>
2016-07-30 23:15:10 +0800

    Update log.py

--- lib/log.py
+++ lib/log.py
@@ -1,22 +1,20 @@
 # -*- coding:utf-8 -*-
 __author__ = 'blfeng'
 import  os
-
 class Collectlog:
     #初始化配置读取的行为0
     def getfile_count(self,configfile,path):
         filecount_name={}
         self.path = path
         self.configfile = configfile
+        #生产配置文件
+        file = open(self.configfile,"w+")
         for root, dirs, files in os.walk(self.path):
             for filename in files:
-                file = open(self.configfile,"a")
-                #count = len(open(root+"/"+filename,'rU').readlines())
                 counts = 0
-                context=root+":"+filename+":"+str(counts)+"\n"
-                file.write(context)
-            file.close()
-    #更新同步后，记录同步行数
+                contexts=(root+"/"+filename+":"+filename+":"+str(counts)+"\n")
+                file.write(contexts)
+        file.close()
     def gencounts(self,configfile,filepath,filename,counts):
         self.configfile = configfile
         self.counts = counts
