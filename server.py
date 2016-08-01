524b924dc745d8a54e6511187ee7360d94214bf3
fengbaoli <wenyefbl@163.com>
2016-08-01 10:31:28 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
 from socket import *
 import struct
-
+import  os,repr
+receive_path="logs"
 while True:
     ADDR = ('0.0.0.0',8000)
     BUFSIZE = 1024
@@ -15,7 +16,14 @@ while True:
     print "客户端已连接—> ",addr
     fhead = conn.recv(FILEINFO_SIZE)
     filename,temp1,filesize,temp2=struct.unpack('128s32sI8s',fhead)
-    filename="text.text"
+    #获取发送端的文件名
+    filename=filename.split('\x00')[0]
+    #创建接收端存放日志路径
+    receive_full_path = os.getcwd()+"\\"+receive_path+"\\"
+    if not os.path.exists(receive_full_path):
+        os.mkdir(receive_full_path)
+
+    filename = receive_full_path+filename
     fp = open(filename,'a')
     restsize = filesize
     print "正在接收文件... ",
