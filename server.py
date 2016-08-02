a067fc9dde09632af83c82fffdb4fcb14044412c
fengbaoli <wenyefbl@163.com>
2016-08-02 13:44:28 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,10 @@
 from socket import *
 import struct
 import  os,repr
+import sys
+sys.path.append(os.getcwd()+'\lib')
+from debug import logdebug
+log = logdebug()
 
 def Collog(port,BUFSIZE,receive_path):
     while True:
@@ -12,21 +16,31 @@ def Collog(port,BUFSIZE,receive_path):
         recvSock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
         recvSock.bind(ADDR)
         recvSock.listen(True)
-        print "等待连接..."
+        message = "等待连接..."
+        log.loginfo(message)
         conn,addr = recvSock.accept()
-        print "客户端已连接—> ",addr
+        network = []
+        for  ipaddr in addr:
+            network.append(ipaddr)
+        message = "客户端已连接—>"+str(ipaddr)
+        log.loginfo(message)
         fhead = conn.recv(FILEINFO_SIZE)
         filename,temp1,filesize,temp2=struct.unpack('128s32sI8s',fhead)
         #获取发送端的文件名
         filename=filename.split('\x00')[0]
+        message = "获取文件:"+filename
+        log.loginfo(message)
         #创建接收端存放日志路径
+        message = "创建接收端存放日志路径"
+        log.loginfo(message)
         receive_full_path = os.getcwd()+"\\"+receive_path+"\\"
         if not os.path.exists(receive_full_path):
             os.mkdir(receive_full_path)
         filename = receive_full_path+filename
         fp = open(filename,'a')
         restsize = filesize
-        print "正在接收文件... ",
+        message = "正在接收文件:"+filename
+        log.loginfo(message)
         while 1:
             if restsize>BUFSIZE:
                 filedata=conn.recv(BUFSIZE)
@@ -38,11 +52,13 @@ def Collog(port,BUFSIZE,receive_path):
             restsize = restsize-len(filedata)
             if restsize == 0:
                 break
-        print "接收文件完毕，正在断开连接..."
+        message= "接收文件 %s 完毕，正在断开连接..." % filename
+        log.loginfo(message)
         fp.close()
         conn.close()
         recvSock.close()
-        print "连接已关闭..."
+        message= "连接已关闭..."
+        log.loginfo(message)
 
 
 
