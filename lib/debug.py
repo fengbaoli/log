4e7a4408d62ed436eed660222369d3039cfb9416
fengbaoli <wenyefbl@163.com>
2016-08-02 13:45:40 +0800

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,22 +1,39 @@
 # -*- coding:utf-8 -*-
-__author__ = '343715'
+__author__ = 'blfeng'
 import logging
 import os
+from Platform import  Platform
+platform = Platform()
+
 
 class logdebug:
     def loginfo(self,message):
         self.message = message
-        logging.basicConfig(level=logging.DEBUG,
+        if platform.isLinuxSystem():
+            logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filename=os.getcwd()+"/logs/collect-info.log",
                     filemode='w')
+        else:
+            logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename=os.getcwd()+"\logs\collect-info.log",
+                    filemode='w')
         logging.info(self.message)
     def logerror(self,message):
         self.message = message
-        logging.basicConfig(level=logging.ERROR,
+        if platform.isLinuxSystem():
+            logging.basicConfig(level=logging.ERROR,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filename=os.getcwd()+"/logs/collect-error.log",
+                    filemode='w')
+        else:
+            logging.basicConfig(level=logging.ERROR,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename=os.getcwd()+"\logs\collect-error.log",
                     filemode='w')
         logging.error(message)
